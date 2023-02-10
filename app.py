a9b81d8e90ed83e27549fb5a95c2b212ab19758b
Harmeet Singh Arora <harmeetudemy@gmail.com>
2023-02-10 06:59:35 +0530

    Cleaned up file app.py to remove extra features initially put in for reference

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
-def reverse_string(string):
-    #return string[::-1]
-    return "Hello"
-
-def ask_gpt(string):
-    #return "Helllllooo"
-    chatgpt.call_gpt(string)
 
 
 from flask import Flask, jsonify, request
 import chatgpt
 app = Flask(__name__)
 
-@app.route("/reverse", methods=["GET"])
-def reverse():
-    string = request.args.get("string")
-    result = reverse_string(string)
-    return jsonify({"result": result})
 
 
 @app.route("/askgpt", methods=["GET"])
